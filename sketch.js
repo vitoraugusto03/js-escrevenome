390c0fe35f09675647796785c36349e2888024da
vitoraugusto03 <vitor.gallo@escola.pr.gov.br>
2024-08-11 19:28:16 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,60 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(222);     
+  }function draw() {
+      background("black");
+      
+      rect(5, 20, 100, 155);
+  }function draw() {
+      background("black");
+  
+      fill("yellow");
+      rect(0, 12, 100, 190);
+  }function draw() {
+      background("black");
+      
+      stroke("gray") 
+      fill("yellow") 
+      rect(0, 12, 100, 190)
+  }function draw(circle) {
+    background(220);
+    
+    stroke("black");
+    fill("yellow");
+    rect(mouseX, mouseY, 100, 150);
+  }function draw(circle) {
+    background(220);
+    
+    stroke("blue");
+    fill("yellow");
+    
+    console.log(mouseIsPressed);
+    rect(mouseX, mouseY, 100, 150);
+  }function setup() {
+    // cria uma tela de 400px de largura por 400px de altura
+    createCanvas(450, 400);
+  }function setup() {
+    createCanvas(400, 400);
+    background("red")
+  }
+  
+  function draw() {
+    stroke("gray");
+    fill('yellow');
+    
+    
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 21, 45);
+    }
+  
+  }
+  
